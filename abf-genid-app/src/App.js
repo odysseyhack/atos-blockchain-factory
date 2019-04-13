67aabc1787b9dfcd8e991a97eb19e7b7d5c7d9e1
Eduardo Leegwater Simões <eduardols@protonmail.com>
2019-04-13 23:49:01 +0200

    Added routing from the Nav

--- abf-genid-app/src/App.js
+++ abf-genid-app/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 import SimpleAppBar from './components/Header';
-import TxtBoxUp from './components/TxtBoxUp'
 import Login from './screens/Login'
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const appTitle = "DigiD+";
 
@@ -16,22 +16,26 @@ const theme = createMuiTheme({
         main: '#CB6500',
       },
     },
+    fontFamily: 'Exo+2',
 });
-
 
 class App extends Component {
 
   render() {
-    
     return (
-      
       <MuiThemeProvider theme={theme}>
         <SimpleAppBar headerTitle={appTitle}/>
-        <Login/>
+        <Router>
+          <Route path="/" exact component={Login}/>
+          <Route path="/id/" component={Login}/>
+          <Route path="/scan/" component={Login}/>
+          <Route path="/profile/" component={Login}/>
+          <Route path="/help/" component={Login}/>
+          <Route path="/settings/" component={Login}/>
         <NavBar/>
+        </Router>
       </MuiThemeProvider>
-      
-      );
+    );
   }
 }
 
--- abf-genid-app/src/screens/Login.js
+++ abf-genid-app/src/screens/Login.js
@@ -27,7 +27,6 @@ class Login extends React.Component {
                     margin="normal"
                     variant="filled"
                 />
-                <Webcam/>
             </div>
         );
     };

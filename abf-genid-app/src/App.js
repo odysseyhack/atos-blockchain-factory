fda2ec79e6c74903bf3a06fc8d53baa4e08309c1
Eduardo Leegwater Simões <eduardols@protonmail.com>
2019-04-14 01:13:46 +0200

    "Fixed" a weird material bar issue

--- abf-genid-app/src/App.js
+++ abf-genid-app/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 import SimpleAppBar from './components/Header';
-import TxtBoxUp from './components/TxtBoxUp'
 import Login from './screens/Login'
 import LoginDigiD from './screens/LoginDigiD'
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import './App.css'
 
 const appTitle = "DigiD+";
 
@@ -30,10 +31,10 @@ class App extends Component {
       <MuiThemeProvider theme={theme}>
         <SimpleAppBar headerTitle={appTitle}/>
         <Router>
-          <Route path="/id" exact component={Login} />
+          <Route path="/" exact component={Login} />
+          <Route path="/id" component={Login} />
           <Route path="/scan/" component={LoginDigiD} />
           <Route path="/profile/" component={LoginDigiD} />
-          <Route path="/help/" component={LoginDigiD} />
           <Route path="/settings/" component={LoginDigiD} />
           <NavBar/>
         </Router>
